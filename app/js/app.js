54d86b566a5ab175922aa3a36ffced4180a8b668
rugead <rudolf.vogel@icando.de>
2017-02-11 21:44:07 +0100

    added flexbox and tomato design

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,14 @@
 'use strict';
 let timeoutID;
+let secondsCounter = 0;
+let minutesCounter =  0;
 let timerOnOff = 'off';
+let moveSec = document.getElementById('moveingSec');
+let moveMin = document.getElementById('moveingMin');
 let m = () => document.getElementById('minutes').innerHTML;
 let s = () => document.getElementById('seconds').innerHTML;
 let tm = () => document.getElementById('timerminutes').innerHTML;
 let bm = () => document.getElementById('breakminutes').innerHTML;
-let rotateDegrees = 0;
 
 let checkTime = (i) => {
     if (i < 10) {i = '0' + i;}  return i;
@@ -15,13 +18,13 @@ let toggleWorkBreak = 'break';
 
 let startTimer = () => {
     if (timerOnOff  === 'pause') {
-      timerOnOff = 'on';
-      let t = parseInt(m()) * 60  + parseInt(s());
-      console.log(t);
-      timer(t);
+        timerOnOff = 'on';
+        let t = parseInt(m()) * 60  + parseInt(s());
+        console.log(t);
+        timer(t);
     }
 
-  if ((timerOnOff === 'off') && (toggleWorkBreak === 'break')) {
+    if ((timerOnOff === 'off') && (toggleWorkBreak === 'break')) {
         timerOnOff = 'on';
         console.log('TIMERONOFF', timerOnOff);
         let sec = parseInt(tm() * 60);
@@ -29,7 +32,7 @@ let startTimer = () => {
         timer(sec);
     }
 
-  if ((timerOnOff === 'off') && (toggleWorkBreak === 'work')) {
+    if ((timerOnOff === 'off') && (toggleWorkBreak === 'work')) {
         timerOnOff = 'on';
         console.log('TIMERONOFF', timerOnOff);
         let sec = parseInt(bm() * 60);
@@ -55,12 +58,11 @@ function toggleTimer() {
 
 let timer = (seconds) => {
     if (seconds <= 0) {
-      toggleTimer();
-      return;
+        toggleTimer();
+        return;
     }
-    rotation(6);
-    console.log('SECONDS', seconds);
-    console.log('TIMEOUTID', toggleWorkBreak);
+
+    moveSec(1.666666);
     seconds = seconds - 1;
     let mB = Math.floor(seconds / 60);
     let sB = seconds % 60;
@@ -69,49 +71,75 @@ let timer = (seconds) => {
     timeoutID = setTimeout(function () { timer(seconds); }, 1000);
 };
 
-let decreaseBreakMinutes = () => {  document.getElementById('breakminutes').innerHTML = parseInt(bm()) - parseInt(1);};
+let decreaseBreakMinutes = () => {  document.getElementById('breakminutes').innerHTML =  checkTime(parseInt(bm()) - parseInt(1));};
 
-let increaseBreakMinutes = () => {  document.getElementById('breakminutes').innerHTML = parseInt(bm()) + parseInt(1);};
+let increaseBreakMinutes = () => {  document.getElementById('breakminutes').innerHTML = checkTime(parseInt(bm()) + parseInt(1));};
 
-let decreaseTimerMinutes = () => {  document.getElementById('timerminutes').innerHTML = parseInt(tm()) - parseInt(1);};
+let decreaseTimerMinutes = () => {
+  let minutes = parseInt(tm()) - parseInt(1);
+  document.getElementById('timerminutes').innerHTML = minutes;
+  document.getElementById('minutes').innerHTML = checkTime(minutes);
+};
 
-let increaseTimerMinutes = () => {  document.getElementById('timerminutes').innerHTML = parseInt(tm()) + parseInt(1);};
+let increaseTimerMinutes = () => {
+  let minutes = parseInt(tm()) + parseInt(1);
+  document.getElementById('timerminutes').innerHTML = minutes;
+  document.getElementById('minutes').innerHTML = checkTime(minutes);
+};
 
 let reset = () =>  {
     var tm = document.getElementById('timerminutes').innerHTML;
-    var bm = document.getElementById('breakminutes').innerHTML;
     clearTimeout(timeoutID);
     timeoutID = undefined;
     timerOnOff = 'off';
-    console.log('stop', timerOnOff);
-    document.getElementById('timerminutes').innerHTML = tm;
-    document.getElementById('breakminutes').innerHTML = bm;
-    document.querySelector(".wedge").style.transform = ('rotateZ(0deg)');
-    rotateDegrees = 0;
+    secondsCounter = 0;
+    document.getElementById('minutes').innerHTML = tm;
+    document.getElementById('seconds').innerHTML = checkTime(0);
+    document.querySelector('#movingSec').style.transform = 'translateY(0%)';
+    document.querySelector('#movingMin').style.transform = 'translateY(0%)';
 };
+
 let startButton = document.getElementById('start-button');
-let stopButton = document.getElementById('stop-button');
-let resetButton = document.getElementById('resetButton');
-
-let increaseBreakButton = document.getElementById('increaseBreakMinutes');
-let decreaseBreakButton = document.getElementById('decreaseBreakMinutes');
-let increaseTimerButton = document.getElementById('increaseTimerMinutes');
-let decreaseTimerButton = document.getElementById('decreaseTimerMinutes');
-
-stopButton.addEventListener('click', function () { stopTimer(); }, false);
-
 startButton.addEventListener('click', function () { startTimer(); }, false);
 
+let stopButton = document.getElementById('stop-button');
+stopButton.addEventListener('click', function () { stopTimer(); }, false);
+
+let resetButton = document.getElementById('resetButton');
+
+moveSec = (x = (1.666666)) => {
+    let d;
+    secondsCounter = secondsCounter + x;
+    if (secondsCounter > 99.5) {
+        secondsCounter = 0;
+        d = 'translateX(' + secondsCounter + '%)';
+
+        moveMin(m());
+    } else {
+        d = 'translateX(' + secondsCounter + '%)';
+    }
+
+    console.log(d);
+    document.querySelector('#movingSec').style.transform = d;
+};
+
+moveMin = (x) => {
+  // document.getElementById('minutes').innerHTML = x - 1;
+  minutesCounter = minutesCounter + 100 / x;
+  let d = 'translateX(' + minutesCounter + '%)';
+  document.querySelector('#movingMin').style.transform = d;
+};
+
+let increaseBreakButton = document.getElementById('increaseBreakMinutes');
+increaseBreakButton.addEventListener('click', function () { increaseBreakMinutes(); }, false);
+
+let increaseTimerButton = document.getElementById('increaseTimerMinutes');
 resetButton.addEventListener('click', function () { reset(); }, false);
 
-increaseBreakButton.addEventListener('click', function (){ increaseBreakMinutes(); rotation(6);}, false);
-decreaseBreakButton.addEventListener('click', function(){ decreaseBreakMinutes(); }, false);
-increaseTimerButton.addEventListener('click', function(){ increaseTimerMinutes(); }, false);
-decreaseTimerButton.addEventListener('click', function(){ decreaseTimerMinutes(); }, false);
+let decreaseBreakButton = document.getElementById('decreaseBreakMinutes');
+decreaseBreakButton.addEventListener('click', function () { decreaseBreakMinutes(); }, false);
 
-// --> rotation
-function rotation (degrees) {
-  rotateDegrees = rotateDegrees + degrees;
-  let d = 'rotate(' + rotateDegrees + 'deg)';
-  document.querySelector(".wedge").style.transform = d;
-}
+increaseTimerButton.addEventListener('click', function () { increaseTimerMinutes(); }, false);
+
+let decreaseTimerButton = document.getElementById('decreaseTimerMinutes');
+decreaseTimerButton.addEventListener('click', function () { decreaseTimerMinutes(); }, false);
